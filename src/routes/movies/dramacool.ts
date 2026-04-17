69444d13aa8d61ede6e02c3ab3c65ac362c1308b
Mark Joseph <imrkjoseph16@Mjs-MacBook-Air-M3.local>
2026-04-17 21:59:07 +0800

    fixed dramacool class

--- src/routes/movies/dramacool.ts
+++ src/routes/movies/dramacool.ts
@@ -8,61 +8,55 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get('/', (_, rp) => {
     rp.status(200).send({
       intro:
-        "Welcome to the flixhq provider: check out the provider's website @ https://flixhq.to/",
+        "Welcome to the DramaCool provider: check out the provider's website @ https://dramacool.com/",
       routes: ['/:query', '/info', '/watch'],
-      documentation: 'https://docs.consumet.org/#tag/flixhq',
+      documentation: 'https://docs.consumet.org/#tag/dramacool',
     });
   });
 
   fastify.get('/:query', async (request: FastifyRequest, reply: FastifyReply) => {
     const query = decodeURIComponent((request.params as { query: string }).query);
+    const page = (request.query as { page: number })?.page || 1;
 
-    const page = (request.query as { page: number }).page;
-
-    const res = await dramacool.search(query, page);
-
-    reply.status(200).send(res);
+    try {
+      const res = await dramacool.search(query, page);
+      reply.status(200).send(res);
+    } catch (err) {
+      console.error(err);
+      reply.status(500).send({ message: 'Search failed', error: String(err) });
+    }
   });
 
   fastify.get('/info', async (request: FastifyRequest, reply: FastifyReply) => {
     const id = (request.query as { id: string }).id;
 
-    if (typeof id === 'undefined')
-      return reply.status(400).send({
-        message: 'id is required',
-      });
+    if (typeof id === 'undefined') {
+      return reply.status(400).send({ message: 'id is required' });
+    }
 
     try {
-      const res = await dramacool
-        .fetchMediaInfo(id)
-        .catch((err) => reply.status(404).send({ message: err }));
-
+      const res = await dramacool.fetchMediaInfo(id);
       reply.status(200).send(res);
     } catch (err) {
-      reply.status(500).send({
-        message:
-          'Something went wrong. Please try again later. or contact the developers.',
-      });
+      console.error(err);
+      reply.status(500).send({ message: 'Failed to fetch media info' });
     }
   });
 
   fastify.get('/watch', async (request: FastifyRequest, reply: FastifyReply) => {
     const episodeId = (request.query as { episodeId: string }).episodeId;
-    // const mediaId = (request.query as { mediaId: string }).mediaId;
-    // const server = (request.query as { server: StreamingServers }).server;
+    const server = (request.query as { server: StreamingServers }).server;
 
-    if (typeof episodeId === 'undefined')
+    if (typeof episodeId === 'undefined') {
       return reply.status(400).send({ message: 'episodeId is required' });
-    try {
-      const res = await dramacool
-        .fetchEpisodeSources(episodeId)
-        .catch((err) => reply.status(404).send({ message: 'Media Not found.' }));
+    }
 
+    try {
+      const res = await dramacool.fetchEpisodeSources(episodeId, server);
       reply.status(200).send(res);
     } catch (err) {
-      reply
-        .status(500)
-        .send({ message: 'Something went wrong. Please try again later.' });
+      console.error(err);
+      reply.status(500).send({ message: 'Something went wrong. Please try again later.' });
     }
   });
 };
